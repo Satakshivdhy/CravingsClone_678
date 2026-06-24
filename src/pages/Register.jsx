d0ed3d6239a87d4f7c50a69bd6e86ecc83a8e525
Satakshivdhy <satakshivaidhy90@gmail.com>
2026-06-25 00:02:29 +0530

    ui of registration page is done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,19 +12,21 @@ const Register = () => {
           <div className='text-center'>Join us as a Customer, Restaurant, or Rider</div>
           <div>
           <label htmlFor="user">Register as:</label><br />
-          <input type="radio" name='user' value='customer'/><span>Customer</span>
-          <input type="radio" name='user' value='restaurant'/><span>Restaurant</span>
-          <input type="radio" name='user' value='rider'/><span>Rider</span>
+          <div className="flex gap-7 ">
+            <div className='flex gap-2'><input type="radio" name='user' value='customer'/><span>Customer</span></div>
+          <div className='flex gap-2'><input type="radio" name='user' value='restaurant'/><span>Restaurant</span></div>
+          <div className='flex gap-2'><input type="radio" name='user' value='rider'/><span>Rider</span></div>
+          </div>
           </div>
           
-          <input type="text" placeholder='Enter your full name' className='p-2 border-[#F2EBE3] rounded border-1 active:border-[var(--accent)]'/>
-          <input type="email" placeholder='Enter your email' />
-          <input type="text" placeholder='Enter your phone number' />
-          <input type="password" placeholder='Enter your password' />
-          <input type="password" placeholder='Confirm your password' />
-          <div><input type="checkbox"/><span>I agree to the terms and conditions.</span></div>
-          <button className='w-full bg-(--primary) rounded p-3'>Register</button>
-          <div>Already registered?<span>Login here</span></div>
+          <input type="text" placeholder='Enter your full name' className='p-2 border-[#F2EBE3] rounded border focus:border-2 focus:border-orange-700 focus:ring-0 focus:outline-none'/>
+          <input type="email" placeholder='Enter your email' className='p-2 border-[#F2EBE3] rounded border focus:border-2 focus:border-orange-700 focus:ring-0 focus:outline-none' />
+          <input type="text" placeholder='Enter your phone number' className='p-2 border-[#F2EBE3] rounded border focus:border-2 focus:border-orange-700 focus:ring-0 focus:outline-none' />
+          <input type="password" placeholder='Enter your password' className='p-2 border-[#F2EBE3] rounded border focus:border-2 focus:border-orange-700 focus:ring-0 focus:outline-none' />
+          <input type="password" placeholder='Confirm your password' className='p-2 border-[#F2EBE3] rounded border focus:border-2 focus:border-orange-700 focus:ring-0 focus:outline-none' />
+          <div><input type="checkbox"/><span> I agree to the <span className='text-(--primary)'>terms and conditions.</span> </span></div>
+          <button className='w-full bg-(--primary) rounded p-3 text-white font-bold'>Register</button>
+          <div className="text-center">Already registered?<span className='text-(--primary) text-semibold '> Login here</span></div>
         </form>
       </div>
     </div>
